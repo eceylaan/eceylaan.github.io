b8886129babd329e961884cd482f01cf5e36b276
eceylaan <ceylann.ece@gmail.com>
2024-05-25 00:15:56 +0300

    ..

--- tahminoyunu/tahmin.js
+++ tahminoyunu/tahmin.js
@@ -19,18 +19,3 @@ if (!secimDogru) {
   console.log('3 hakkınız da bitti. Kaybettiniz. Doğru tahmin ' + sayi + ' olacaktı.')
 }
 
-
-
-
-//for (let i = 1; i <= 3; i++) {
-//  let tahmin = Number(prompt(i + ' . tahmininizi girin: '))
-//}
-
-
-// let sayiAdedi = prompt("Kaç adet sayı toplamak istiyorsunuz?");
-// let toplam = 0
-
-// for (let i = 1; i <= sayiAdedi; i++) {
-//   toplam += Number(prompt(i + ' . sayıyı giriniz : '))
-// }
-// alert('sonuç : ' + toplam);
